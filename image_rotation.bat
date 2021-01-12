7f22f5c7f50fc288bd9be3c6df93da9cc3461269
Thisara Chathuranga <54197530+Thisacha@users.noreply.github.com>
2021-01-13 00:42:28 +1100

    Update image_rotation.bat
    Added Progress monitoring, No manual changes required - all the processed images can be found in the processed folder, Improved image naming convention

--- image_rotation.bat
+++ image_rotation.bat
@@ -1,27 +1,37 @@
 @echo off
-set root=replace with your path
+set root=Replace this text with your path
 CD /D %root%
-MD cropped
-MD processed 
-magick mogrify -gravity center -crop 16:9 +repage -path cropped *.jpg
-CD cropped
-MD resized
-magick mogrify -resize 1280x720 -gravity center -path resized *.jpg
-CD resized
-MD rotated
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 15 rotated\deg15_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 30 rotated\deg30_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 45 rotated\deg45_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 60 rotated\deg60_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 75 rotated\deg75_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 90 rotated\deg90_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 105 rotated\deg105_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 120 rotated\deg120_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 135 rotated\deg135_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 150 rotated\deg150_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 165 rotated\deg165_%%a
-FOR %%a in (*.jpg) DO magick %%a -background black -rotate 180 rotated\deg180_%%a
-CD rotated
-magick mogrify -gravity center -background black -extent 1280x720 -crop 1280x720 *.jpg
+set /a Index=1
+setlocal enabledelayedexpansion
+for /r %%i in (*.jpg) do ( 
+    rename "%%i" "!Index!.jpg"
+    set /a Index+=1
+)
+endlocal
+MD Processed
+magick mogrify -monitor -gravity center -crop 16:9 +repage -path Processed *.jpg
+CD Processed
+magick mogrify -monitor -resize 1280x720 -gravity center *.jpg
+MD Rotated
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 15 Rotated\%%~na"_deg_15"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 30 Rotated\%%~na"_deg_30"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 45 Rotated\%%~na"_deg_45"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 60 Rotated\%%~na"_deg_60"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 75 Rotated\%%~na"_deg_75"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 90 Rotated\%%~na"_deg_90"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 105 Rotated\%%~na"_deg_105"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 120 Rotated\%%~na"_deg_120"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 135 Rotated\%%~na"_deg_135"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 150 Rotated\%%~na"_deg_150"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 165 Rotated\%%~na"_deg_165"%%~xa
+FOR %%a in (*.jpg) DO magick %%a -monitor -background black -rotate 180 Rotated\%%~na"_deg_180"%%~xa
+CD Rotated 
+magick mogrify -monitor -gravity center -background black -extent 1280x720 -crop 1280x720 *.jpg
+SET src_folder=%root%\Processed\Rotated
+SET tar_folder=%root%\Processed
+for /f %%a IN ('dir "%src_folder%" /b') do move "%src_folder%\%%a" "%tar_folder%\"
+CD ..
+rmdir Rotated
+cls
+echo Finished Processing - Please check the Processed folder
 PAUSE
-
